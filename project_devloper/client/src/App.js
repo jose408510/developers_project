6eb8cd08941b3a1c63d07d02f6dfbeecee7a8b82
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-19 13:19:56 -0800

    added react-router

--- project_devloper/client/src/App.js
+++ project_devloper/client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BroweserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>My react app</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+            <Route exact path="/" Component={ Landing } /> 
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
