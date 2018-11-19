e8983357583c198789df030081675928f66bc3f3
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-19 15:03:50 -0800

    added register

--- project_devloper/client/src/App.js
+++ project_devloper/client/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { BroweserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 
 class App extends Component {
   render() {
@@ -11,7 +16,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-            <Route exact path="/" Component={ Landing } /> 
+            <Route exact path="/" component={ Landing } /> 
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div> 
           <Footer/>
         </div>
       </Router>
