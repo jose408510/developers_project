110e19c9cd326d8a5e379ebf0738235ba3dda7a5
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-20 17:34:35 -0800

    added textfield and modified register and login

--- project_devloper/client/src/App.js
+++ project_devloper/client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken'
-import { setCurrentUser } from './actions/authActions'
+import { setCurrentUser , logoutUser } from './actions/authActions'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -14,14 +14,24 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import './App.css';
 
-//check for token
-if(localStorage.jwt) {
-  //Set auth token header auth
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  //decode token and get user info and exp 
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
-  //set user and is authenticated
-  store.dispatch(setCurrentUser(decoded))
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 
