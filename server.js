f876e849d49e446c7fab996ce92d34e7ffcf6d2d
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-26 20:20:11 -0800

    just prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const passport = require('passport');
+const path = require('path')
 
 const app = express();
 const users = require('./routes/api/users');
@@ -32,7 +33,13 @@ app.use('/api/profiles',profile)
 app.use('/api/posts', posts)
 
 // Server Static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
+    app.get('*', (req , res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'))
+    })
+}
 // so you can deploy on heroku you add process.env.port
 const port = process.env.PORT || 5000 
 
